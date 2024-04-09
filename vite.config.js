d99c4cfc455e53b89cbad236114b3f606c00f4ef
sumitk@deqode.com <sumitk@deqode.com>
2024-04-09 18:05:08 +0530

    maded changes to congif

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    // vite config
-    define: {
-      ...Object.keys(env).reduce((prev, key) => {
-        prev[`process.env.${key}`] = JSON.stringify(env[key]);
-        return prev;
-      }, {}),
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://quizapp-backend-kg6h.onrender.com/",
     },
-    server: {
-      proxy: {
-        "/api": env.API_URL,
-      },
-    },
-  };
+  },
 });
